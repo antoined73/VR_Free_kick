c71aa0dc1e496d6c67f54ca2dc792984ff2b5c03
Kariamoss <jehanmilleret@gmail.com>
2019-01-17 23:22:51 +0100

    Menu and multiple cameras depending on role

---
+++ unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Camera> cameras;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void choiceRole(string role)
+    {
+        disableAllCameras();
+        if(role == "buteur")
+        {
+            cameras[0].enabled = true;
+        } else if (role == "mur")
+        {
+            cameras[2].enabled = true;
+        } else if (role == "gardien")
+        {
+            cameras[1].enabled = true;
+        }
+    }
+
+    private void disableAllCameras()
+    {
+        foreach(Camera camera in cameras)
+        {
+            camera.enabled = false;
+        }
+    }
+}
