02b507627c8bff0a086082f83ffcac8222b6807c
Unknown <marcjourdes@gmail.com>
2019-01-18 08:56:12 +0100

    Defender scripts and first model

---
+++ unity/Assets/Scripts/BuildWall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BuildWall : MonoBehaviour
+{
+
+    public Transform defender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            float position = x - 2.5f;
+            Instantiate(defender, new Vector3(position + defender.localScale.x, 0, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ unity/Assets/Scripts/DefenderMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DefenderMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float jumpForce = 200f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, jumpForce * Time.deltaTime, 0), ForceMode.Impulse);
+        }
+        rb.AddForce(Vector3.down * Time.deltaTime);
+    }
+}
