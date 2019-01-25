7bc59ec8d3c189cfd749a32ded8ee493458d9ca2
Antoine Dezarnaud <dezartoine@hotmail.fr>
2019-01-25 15:45:42 +0100

    Add replay to attach on object and big screen for goal

--- unity/Assets/Scripts/GameManager.cs
+++ unity/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,6 +8,19 @@ public class GameManager : MonoBehaviour
     public List<Camera> cameras;
 
     private Role roleChoosen;
+    private Replay[] objectsTransform;
+
+    private void Awake()
+    {
+        objectsTransform = (Replay[]) FindObjectsOfType(typeof(Replay));
+        objectsTransform[0].startRecording();
+        Debug.Log("start");
+        Task.Delay(5000).ContinueWith((task) => {
+            Debug.Log("stop");
+            objectsTransform[0].stopRecording();
+            objectsTransform[0].startPlayingBack();
+        });
+    }
 
     public void choiceRole(Role role)
     {
---
+++ unity/Assets/Scripts/Replay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replay : MonoBehaviour
+{
+    private new Transform transform;
+    private bool isRecording = false;
+    private bool isPlayingBack = false;
+    private List<Vector3> transformPosition;
+    private List<Quaternion> transformRotation;
+    private int currentLoop = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isRecording)
+        {
+            transformPosition.Add(transform.position);
+            transformRotation.Add(transform.rotation);
+        } else if (isPlayingBack && currentLoop < transformPosition.Count - 1)
+        {
+            transform.position = transformPosition[currentLoop++];
+            transform.rotation = transformRotation[currentLoop++];
+        }
+    }
+
+    public void startRecording()
+    {
+        transformPosition = new List<Vector3>();
+        transformRotation = new List<Quaternion>();
+        isRecording = true;
+    }
+
+    public void stopRecording()
+    {
+        isRecording = false;
+    }
+
+    public void startPlayingBack()
+    {
+        currentLoop = 0;
+        isPlayingBack = true;
+    }
+
+    public void stopPlayingBack()
+    {
+        isPlayingBack = false;
+
+    }
+}
