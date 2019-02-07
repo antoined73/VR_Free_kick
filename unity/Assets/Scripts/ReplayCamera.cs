403bec8ba8572e07ccfe0cc2b873506b7ab6cf70
Kariamoss <jehanmilleret@gmail.com>
2019-02-07 23:42:11 +0100

    Add camera following ball

---
+++ unity/Assets/Scripts/ReplayCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayCamera : MonoBehaviour
+{
+    public GameObject ball;
+
+    public Vector3 offset;
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the ball's, but offset by the calculated offset distance.
+        transform.position = ball.transform.position + offset;
+    }
+}
